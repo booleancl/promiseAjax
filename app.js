b67e9de7d21fb3fd183ea324aba2b82687a5f802
sbstnjmnz <sbstnjmnz@yahoo.com>
2022-02-19 09:56:42 -0300

    incomplete version

--- app.js
+++ app.js
@@ -21,50 +21,20 @@ const PokeApp = {
   },
   
   makeRequest(url, method) {
-    
-    const request = new XMLHttpRequest();
-    
-    return new Promise(function (resolve, reject) {
-      
-      request.onreadystatechange = function () {
-        if (request.readyState !== 4) return;
-        
-        if (request.status >= 200 && request.status < 300) {
-          resolve(request.response);
-        } else {
-          reject({
-            status: request.status,
-            statusText: request.statusText
-          });
-        }
-      };
-      
-      request.open(method || 'GET', url, true);
-      request.send();
-    });
+    //...
   },
 
   getPokemon(name){
-    this.makeRequest(`${this.baseURL}pokemon/${name}`)
-    .then(pokemon => { 
-      return {
-        name,
-        image: JSON.parse(pokemon).sprites.front_default
-      }
-    })
-    .then(pokeInfo => this.renderCard(pokeInfo))
+    //...
   },
 
   async getAllPokemon(event){
-    event.preventDefault()
-    const response = await this.makeRequest(`${this.baseURL}pokemon?limit=50`)
-    const pokemons = JSON.parse(response).results
-    pokemons.forEach( poke => { this.getPokemon(poke.name) })
+    //...
   },
   
   init(){
     this.button = document.getElementById(this.buttonId)
-    this.button.addEventListener('click', async (event) => this.getAllPokemon(event))
+    this.button.addEventListener('click', (event) => this.getAllPokemon(event))
   }
 }
 
